09ca713add63ba2e9f9aa8fe9e06e054771d1215
Ivanov_Serhiy <s.ivanov@flex-solution.com>
2026-01-29 12:25:03 +0200

    Fixed boot project start

--- src/main/java/com/example/demo/InterviewPrompterApplication.java
+++ src/main/java/com/example/demo/InterviewPrompterApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class InterviewPrompterApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(InterviewPrompterApplication.class, args);
 	}
 
 }
